037f017bbf00ce0f8c82aff5cbb72fe5ddbc8db4
mohammad eydi <mohammadeydi2020@gmail.com>
2022-08-03 19:17:38 +0430

    loop training 2

--- js/secoundTrainingjs.js
+++ js/secoundTrainingjs.js
@@ -1,11 +1,23 @@
-const Array = ["mohammad-09364826426","reza-09214225731","ali-09210781758"]
-const searchedName = prompt("search the name")
-// console.log(searchedName);
-for (let i = 0; i < Array.length; i++) {
-    let splitedName = Array[i].split("-")
+// const Array = ["mohammad-09364826426","reza-09214225731","ali-09210781758"]
+//  const searchedName = prompt("search the name")
+// // console.log(searchedName);
+// for (let i = 0; i < Array.length; i++) {
+//     let splitedName = Array[i].split("-")
    
-   if (Array[i].includes(searchedName)) {
-        console.log(`the name number is ${splitedName[1]}`);
-        break;
+//    if (Array[i].includes(searchedName)) {
+//         console.log(`the name number is ${splitedName[1]}`);
+//         break;
+//     }
+// }
+ 
+//برنامه ای بنویسید که اعدااد  1 تا 30 را که جذر ساده دارند چاپ کنند
+
+const num = prompt("Enter your number !")
+for (let i = 1; i < num; i++) {
+    let jazr = Math.sqrt(i)
+    if (Math.floor(jazr) !== jazr) {
+       continue;
+     
     }
+     console.log(i)
 }
