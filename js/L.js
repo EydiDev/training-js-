9e9765bd355ec760c15aab45222b7f574c5a0142
mohammad eydi <mohammadeydi2020@gmail.com>
2022-08-12 18:45:17 +0430

    gir nade

--- js/L.js
+++ js/L.js
@@ -5,7 +5,10 @@ console.log(heads);
 const anchors = document.getElementsByClassName("anchor")
 console.log(anchors);
 anchors[0].text = "heelo"
-const querys = document.querySelectorAll(".anchor")
+const querys = document.querySelectorAll(".anchor");
+querys.forEach(item => {
+    item.style.color = "Red";
+});
 console.log(querys);
 
  
@@ -14,4 +17,11 @@ console.log(querys);
 const firstDiv = document.getElementById("fistdiv")
 console.log(firstDiv);
 const secDiv = document.querySelector("#secdiv")
+secDiv.style.backgroundColor = " silver" ;
+secDiv.style.color= "purple"
 console.log(secDiv);
+
+
+document.getElementsByClassName("anchor")[0].style.color = "red"
+console.log();
+
