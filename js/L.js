024d63cb0a1dd1eca5d96ffa132307568e7bc8af
mohammad eydi <mohammadeydi2020@gmail.com>
2022-08-14 02:20:29 +0430

    append child replace child and remove child and removed trained

--- js/L.js
+++ js/L.js
@@ -33,3 +33,26 @@ console.log(ul.previousElementSibling);
 console.log(ul.nextElementSibling);
 ul.lastElementChild.style.color="red"
 console.log(ul.parentElement);
+
+//append and delete
+const ele = document.createElement('div')
+const x = document.getElementById("eleDiv")
+console.log(ele);
+ele.id ="eleDivChild";
+ele.className = "class1 class2"
+ele.setAttribute("list-style" , "[1][2][3]")
+ele.setAttribute("style" , "font-style : italic ; font-size : 24px")
+ele.innerText= " hello"
+// x.appendChild(ele)
+const old =document.getElementById("head")  ; 
+const container = document.getElementById("fistdiv").nextElementSibling;
+// console.log(container);
+
+// replace
+container.replaceChild(ele , old);
+// container.remove()
+const hhh = document.getElementById("hh")
+const lastdiv = document.getElementById("lastDiv")
+console.log(lastdiv);
+lastdiv.removeChild(lastdiv.children[0]);
+// lastdiv.remove()
