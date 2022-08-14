c8bae3361caea5912e6325aa59d652fbf96b3549
mohammad eydi <mohammadeydi2020@gmail.com>
2022-08-14 08:52:50 +0430

    maked a mashin hesab in fifth  training

---
+++ js/fifthtraining.js
@@ -0,0 +1,34 @@
+let displayBox = document.querySelector('#resultinput');
+const sd = document.querySelectorAll(".showDisplay")
+const ac = document.querySelector(".all-clear")
+const calc = document.querySelector(".calculator")
+const cl = document.querySelector(".clear-last")
+
+sd.forEach(item => {
+    item.addEventListener("click", function (e) {
+
+        // console.log(item);
+        // console.log(e);
+        displayBox.value += item.innerText;
+      })
+});
+
+
+ac.addEventListener("click" , function (e) {
+    displayBox.value = "";
+})
+
+calc.addEventListener("click" , function (e) {
+  let result = displayBox.value;
+  displayBox.value = eval(result)
+
+  })
+cl.addEventListener("click", function (e) {
+    if (displayBox.value.length === 1) {
+        displayBox.value = "";
+    }
+    else{
+        displayBox.value = displayBox.value.substring(0 ,displayBox.value.length-1 )
+    }
+  })
+  console.log(displayBox.value);
