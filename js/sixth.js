be4523e223e93d545dfd738d48bc801a72c09491
mohammad eydi <mohammadeydi2020@gmail.com>
2022-08-17 13:27:56 +0430

    hang man training ended :))))))))

--- js/sixth.js
+++ js/sixth.js
@@ -4,33 +4,32 @@ const horoof = document.querySelectorAll('.td')
 
 let clicked = [];
 let result = "";
-
 const w = document.getElementById("W");
-
 const image = document.getElementById("photo")
+let mistakes = 0;
 
 function selectRandomItem() {
     randomItem = kalameha[Math.floor(Math.random()*kalameha.length)]
         console.log(randomItem);
     horoof.forEach(item => {
         item.addEventListener("click" , buttonHandler)
+        
     });
    
-  }
-
-
-
-
-
+}
 
 function letterHandler(letter) {
     clicked.indexOf(letter) === -1 ? clicked.push(letter) : null ;
     if (randomItem.includes(letter)) {
           setUnderscores();
+          checkIfWon();
     }else if (!randomItem.includes(letter)){
-        
+        mistakes++;
+        checkIfLost();
+        updateHangmanPicture()
     }
 }
+
 function setUnderscores() {
     let splitedWord = randomItem.split("");
     console.log(splitedWord);
@@ -40,22 +39,34 @@ function setUnderscores() {
     w.innerText=result;
 
 }
-
-function chickIfWon() {
+function checkIfWon() {
     if (randomItem === result) {
-      document.getElementById("go").style.display = "block";
-
-       
+      document.getElementById("go").innerText = "you won!";
+      document.getElementById("keyboard").style.display = "none "; 
     }
 }
 
-function buttonHandler(event) { 
+function checkIfLost() {
+    if (mistakes === 6) {
+        console.log("Ddd");
+        document.getElementById("go").innerText = "you lost!";
+       document.getElementById("wi").innerHTML = `the word was = ${randomItem}`
+       document.getElementById("keyboard").style.display = "none "; 
+    }
+}
+function updateHangmanPicture() {
+    const image = document.getElementById("photo");
+    image.src = `../assets/hangman${mistakes}.png`
+}
 
+function buttonHandler(event) { 
+    
     event.target.className = "clck"
     letterHandler(event.target.innerText)
     console.log(event.target.innerText);
-chickIfWon();
- }
+    
+    
+}
 
  selectRandomItem();
  setUnderscores();
