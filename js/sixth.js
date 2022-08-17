ce150427621474d5640cc7f1436e0a648f39b0b9
mohammad eydi <mohammadeydi2020@gmail.com>
2022-08-17 12:40:47 +0430

    sixth training updated

--- js/sixth.js
+++ js/sixth.js
@@ -6,18 +6,18 @@ let clicked = [];
 let result = "";
 
 const w = document.getElementById("W");
-console.log(horoof);
 
+const image = document.getElementById("photo")
 
 function selectRandomItem() {
     randomItem = kalameha[Math.floor(Math.random()*kalameha.length)]
-    console.log(randomItem);
+        console.log(randomItem);
     horoof.forEach(item => {
         item.addEventListener("click" , buttonHandler)
     });
-
+   
   }
-selectRandomItem();
+
 
 
 
@@ -28,25 +28,35 @@ function letterHandler(letter) {
     if (randomItem.includes(letter)) {
           setUnderscores();
     }else if (!randomItem.includes(letter)){
-
+        
     }
 }
 function setUnderscores() {
     let splitedWord = randomItem.split("");
     console.log(splitedWord);
-    let mappedWord = splitedWord.map(letter => (clicked.indexOf(letter) >= 0 ? letter : " _ "))
+    let mappedWord = splitedWord.map(letter => (clicked.indexOf(letter) >= 0 ? letter : " _"))
     console.log(mappedWord);
     result = mappedWord.join("");
     w.innerText=result;
 
 }
 
+function chickIfWon() {
+    if (randomItem === result) {
+      document.getElementById("go").style.display = "block";
+
+       
+    }
+}
 
 function buttonHandler(event) { 
 
     event.target.className = "clck"
     letterHandler(event.target.innerText)
     console.log(event.target.innerText);
-  
+chickIfWon();
  }
+
+ selectRandomItem();
+ setUnderscores();
 
