c15edee243f87bb48b00cf3657e61a0d66508227
mohammad eydi <mohammadeydi2020@gmail.com>
2022-08-18 19:47:01 +0430

    todolist html and css maked

---
+++ js/todolistTraining.js
@@ -0,0 +1,11 @@
+const submit = document.getElementById("sub");
+
+const text =  document.getElementById("todoTextInput").value;
+submit.addEventListener("click" , subclicked)
+
+
+
+function subclicked() {
+   
+
+}
