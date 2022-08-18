9ca8b22096db0999e70c22e366c49548aa343579
mohammad eydi <mohammadeydi2020@gmail.com>
2022-08-19 00:52:28 +0430

    ui created completly

--- js/todolistTraining.js
+++ js/todolistTraining.js
@@ -7,10 +7,25 @@ submit.addEventListener("click" , subclicked)
 
 
 function subclicked() {
- let newEl =   document.createElement("li");
+ const newElDiv =   document.createElement("div");
+ newElDiv.className= "newElDiv";
+ ul.appendChild(newElDiv)
+
+ const newEl =   document.createElement("li");
  newEl.className= "li";
  newEl.innerText=text.value;
  
  console.log(newEl);
- ul.appendChild(newEl)
+ newElDiv.appendChild(newEl)
+text.value = "";
+
+const completedButton = document.createElement("button")
+completedButton.innerHTML = "<i class='fa-solid fa-check'></i>";
+completedButton.classList.add("completeButton")
+newElDiv.appendChild(completedButton)
+
+const deleteButton = document.createElement("button")
+deleteButton.innerHTML = "<i class='fa-solid fa-trash'></i>";
+deleteButton.classList.add("deleteButton")
+newElDiv.appendChild(deleteButton)
 }
