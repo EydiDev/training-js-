cf45b0080f5efb3a89dde8cc5e4515c042a4dbf9
mohammad eydi <mohammadeydi2020@gmail.com>
2022-08-18 20:22:37 +0430

    make todo list created ما میتوانیم تا این مرحله برنامه را نوشته و ادد کنیم

--- js/todolistTraining.js
+++ js/todolistTraining.js
@@ -1,11 +1,16 @@
 const submit = document.getElementById("sub");
-
-const text =  document.getElementById("todoTextInput").value;
+const ul = document.getElementById("ul")
+const text =  document.getElementById("todoTextInput");
+console.log(text);
 submit.addEventListener("click" , subclicked)
 
 
 
 function subclicked() {
-   
-
+ let newEl =   document.createElement("li");
+ newEl.className= "li";
+ newEl.innerText=text.value;
+ 
+ console.log(newEl);
+ ul.appendChild(newEl)
 }
