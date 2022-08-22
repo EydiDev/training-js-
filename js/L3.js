b1a6b72d1b605ae86e56976121f283b188cbbab6
mohammad eydi <mohammadeydi2020@gmail.com>
2022-08-22 17:05:16 +0430

    Map trained

--- js/L3.js
+++ js/L3.js
@@ -14,3 +14,12 @@ console.log(getedData);
 parsedData = JSON.parse(getedData)
 console.log(parsedData);
 localStorage.clear();
+
+
+
+//Map this is diffrent with map ; Map is like object but we can set object for key
+const myMap = new Map(); 
+myMap.set( {name : "mohammad" , family : "eydi"}, 09219872433
+)   
+console.log(myMap);
+
