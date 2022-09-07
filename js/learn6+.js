d02762359079d95f7489b954a5492a11f53f7f07
mohammad eydi <mohammadeydi2020@gmail.com>
2022-09-07 17:35:03 +0430

    closure learned

--- js/learn6+.js
+++ js/learn6+.js
@@ -17,3 +17,62 @@ try {
     }catch(error) {
         console.log("Error");
     }
+console.log("-------------------");
+//closure 
+let num = 0 ;
+function nam() {
+    num++;
+return num;
+}
+ 
+console.log( nam());
+console.log( nam());
+console.log( nam());
+console.log( nam());
+console.log("-------------------");
+function nam2() { 
+    num++;
+    return num;
+ }
+ console.log( nam2());
+ console.log( nam2());
+ console.log( nam2());
+ console.log( nam2());
+console.log("-------------------");
+ //در متغیر گلوبال وقتی ما در یک فانکشن ان متغیر را تغیر میدهیم متغیر کلا عوض میشود در اخر 
+ function nam3() {
+     let num2 = 0;
+     num2++;
+     return num2;
+   }
+   console.log( nam3());
+   console.log( nam3());
+   console.log( nam3());
+console.log("-------------------");
+   //متغیر لوکال هم هر بار که فانکشن تمام میشه دوباره از اول تعریف میشه و دیتای قبلی حذف میشه 
+   //و با این دو طریق ما نمیتوانیم تعداد اجرا های هر فانکشن استفاده کنیم 
+   //پس مجبوریم از closure استفاده کنیم
+
+
+   function closure() { 
+       let num3 = 0;
+       function nam4() { 
+           num3++;
+           return num3;
+        }
+        return nam4;
+    }
+    const data = closure();
+
+    console.log(data());
+    console.log(data());
+    console.log(data());
+    console.log(data());
+console.log("-------------------");
+
+    const count = closure();
+    console.log(count());
+    console.log(count());
+    console.log(count());
+    console.log(count());
+// end of closure 
